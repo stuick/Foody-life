db1230cded0c45a5388cc6ac19f1172e83e06232
Ahmedsidik Diallo <stuick@gmail.com>
2023-06-29 12:11:19 -0700

    Optimize imports extract strings resource

--- app/src/main/java/com/example/foodylife/DessertsActivity.java
+++ app/src/main/java/com/example/foodylife/DessertsActivity.java
@@ -1,13 +1,10 @@
 package com.example.foodylife;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DessertsActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/foodylife/MainActivity.java
+++ app/src/main/java/com/example/foodylife/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.foodylife;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ListView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
 
